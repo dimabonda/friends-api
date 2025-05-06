52a74038ad2d325fdc0f315575c246bc44a9ccaf
dimabonda <dimabondarenko2404@gmail.com>
2025-05-06 23:53:52 +0300

    upd

--- src/api/comment/controllers/comment.ts
+++ src/api/comment/controllers/comment.ts
@@ -61,19 +61,81 @@ export default factories.createCoreController('api::comment.comment', ({ strapi}
         }
     },
 
-    // async getComments(ctx: Context){
-    //     try {
-    //         const { postId } = ctx.params;
+    async getComments(ctx: Context){
+        try {
+            const { postId } = ctx.params;
+            const pageSize = Array.isArray(ctx.query.pageSize) ? ctx.query.pageSize[0] : ctx.query.pageSize || "10";
+            const lastCommentId = Array.isArray(ctx.query.lastCommentId) ? ctx.query.lastCommentId[0] : ctx.query.lastCommentId || null;
 
-    //         const comments = await strapi.service('api::comment.comment').getComments(postId);
+            const user = await strapi.plugin("users-permissions").service("user-service").getUserByRequest(ctx);
 
-    //         ctx.send({
-    //             message: "Comments retrieved successfully",
-    //             data: comments,
-    //         }, 200)
-    //     } catch (error) {
-    //         console.error("Error", error.message);
-    //         ctx.internalServerError("An unexpected error occurred" )
-    //     }
-    // }
+            if(!user){
+                return ctx.notFound("User not found")
+            }
+
+            if(!user.confirmed){
+                return ctx.badRequest("User account is not confirmed.");
+            }
+    
+            if(user.blocked){
+                return ctx.badRequest("User account is blocked.");
+            }
+
+            if(!postId){
+                return ctx.badRequest("Post ID is required")
+            }
+
+            const post = await strapi.service('api::post.post').getPostById(postId);
+
+            if(!post){
+                return ctx.notFound("Post not found");
+            }
+
+            const comments = await strapi.service('api::comment.comment').getComments(lastCommentId, pageSize, postId);
+
+            ctx.send({
+                message: "Comments retrieved successfully",
+                data: comments,
+            }, 200)
+        } catch (error) {
+            console.error("Error", error.message);
+            ctx.internalServerError("An unexpected error occurred" )
+        }
+    },
+
+    async likeComment(ctx: Context) {
+        try {
+            const { commentId } = ctx.params;
+
+            const user = await strapi.plugin("users-permissions").service("user-service").getUserByRequest(ctx);
+
+            if (!user) {
+                return ctx.notFound("User not found");
+            }
+
+            if (!user.confirmed) {
+                return ctx.badRequest("User account is not confirmed.");
+            }
+
+            if (user.blocked) {
+                return ctx.badRequest("User account is blocked.");
+            }
+            
+            const comment = await strapi.service('api::comment.comment').getCommentById(commentId);
+
+            if (!comment) {
+                return ctx.notFound("Comment not found");
+            }
+
+            const response = await strapi.service('api::comment.comment').likeComment(comment, user);
+
+            ctx.send({
+                message: response.message,
+                data: response.updatedComment,
+            }, 200);
+        } catch (error) {
+            console.error("Error", error.message);
+            ctx.internalServerError("An unexpected error occurred")
+        }
+    }
 }));
